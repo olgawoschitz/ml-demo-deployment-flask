531d2471040d6fd21517212d419493de33103b37
Olga Zharikova <o.zharikova@campus.tu-berlin.de>
2020-03-10 12:24:22 +0100

    Added and reformat tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,4 @@
 import pytest
-import json
 from webApp.heart_disease_webApp import app
 
 
@@ -16,19 +15,26 @@ def test_get(client):
 
 def test_post(client):
     response = client.post("/predictions", data={
-        "age": 23,
-         "sex": 1,
-         "cp": 1,
-         "trestbps": 120,
-         "chol": 124,
-         "fbs": 1,
-         "restecg": 1,
-         "thalach": 173,
-         "exang": 0,
-         "oldpeak": 1,
-         "slope": 2,
-         "ca": 1,
-         "thal": 3
+        "age": 34,
+        "sex": 1,
+        "cp": 0,
+        "trestbps": 120,
+        "chol": 124,
+        "fbs": 0,
+        "restecg": 1,
+        "thalach": 130,
+        "exang": 1,
+        "oldpeak": 1,
+        "slope": 2,
+        "ca": 0,
+        "thal": 3
     })
-    assert "200 OK" in response.status
-    assert b"It is 1" in response.data
+    assert b"There is 0.31% chance of having a heart disease." in response.data
+
+
+def test_missing_values(client):
+    response = client.post("/predictions", data={
+        "age": 23,
+        "sex": 1
+    })
+    assert b"Predicting not possible, all features needed." in response.data
